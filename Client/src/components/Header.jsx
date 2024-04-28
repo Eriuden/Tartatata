b95e655005070d3dcee05cb0fb358743c96a9e63
Eriuden <antoinecarrette@hotmail.fr>
2024-04-28 09:04:38 +0200

    Header 1

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom'
+import {Squash as Hamburger} from "hamburger-react"
+
+// La nav : les types de tartes avec un link pour y amener
 
 export const Header = () => {
+  const [hamburger, setHamburger] = useState(false)
+
   return (
-    <div>Header</div>
+    <div>
+      <div>
+        <h1>Tartatata</h1>
+        <p>Une bonne tarte dans la bouche</p>
+      </div>
+
+      <nav className='hidden flex-row justify-around mt-0 sm:flex'>
+        <Link to={"/"}>Pizzas</Link>
+        <Link to={"/"}>Quiches</Link>
+        <Link to={"/"}>Tielles</Link>
+        <Link to={"/"}>Flammekueches</Link>
+        <Link to={"/"}>Desserts</Link>     
+      </nav>
+
+      <h2 className='flex m-3 sm:hidden' onClick={()=> setHamburger(!hamburger)}>
+        <Hamburger/>
+      </h2>
+
+      { hamburger ? (
+        <nav className='flex flex-col items-start justify-start
+        border-spacing-1 ml-3.5 absolute border-2 border-black
+        opacity-100 sm:hidden'>
+          <Link to={"/"}>Pizzas</Link>
+          <Link to={"/"}>Quiches</Link>
+          <Link to={"/"}>Tielles</Link>
+          <Link to={"/"}>Flammekueches</Link>
+          <Link to={"/"}>Desserts</Link> 
+        </nav>
+      ) :""}
+    </div>
   )
 }
